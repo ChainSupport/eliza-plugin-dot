c0ff5a3a2392b5c188cb478163222bb901fa2e0c
weimeme <weimeme067@gmail.com>
2025-11-07 18:54:47 +0800

    update plugin

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { MY_WALLET_HISTORY } from './actions/my-wallet-history';
 import { TRANSFER_ASSETS } from './actions/assets-transfer';
 import { SEND_MESSAGE } from './actions/send-message';
 import { AssetHubService } from './assethub-service';
-import {GET_MY_WALLET_INFO} from "./actions/get-my-wallet-info"
-
+import {GET_MY_WALLET_INFO} from "./actions/get-my-wallet-info";
 
 export const polkadotAssetHubPlugin: Plugin = {
   name: "POLKADOT_ASSET_HUB",
@@ -15,6 +14,10 @@ export const polkadotAssetHubPlugin: Plugin = {
   evaluators: [],
   services: [AssetHubService],
   actions: [USER_ASSETS_BALANCE, MY_WALLET_HISTORY, TRANSFER_ASSETS, SEND_MESSAGE, GET_MY_WALLET_INFO],
+
+  init: async (_, runtime) => {
+    console.log('Polkadot Asset Hub plugin initialized.');
+  }
 };
 
 export default polkadotAssetHubPlugin;
