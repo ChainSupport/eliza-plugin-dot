0db800f1c45a96eb994d4acc7f1278124fdc8c26
weimeme <weimeme067@gmail.com>
2025-11-08 12:53:40 +0800

    update

--- src/actions/assets-transfer.ts
+++ src/actions/assets-transfer.ts
@@ -64,18 +64,18 @@ interface TransferContent extends Content {
  */
 function validateTransferContent(runtime: IAgentRuntime, content: TransferContent): boolean {
 
-    if (content.recipient === null || !checkAddress(content.recipient.trim(), 0)[0]) {
+    if (content.recipient == null || !checkAddress(content.recipient.trim(), 0)[0]) {
         runtime.logger.warn(`recipient ${content.recipient} is not a valid address`);
         return false;
     }
 
-    if (content.amount === null) {
+    if (content.amount === null || content.amount === undefined) {
         runtime.logger.warn(`amount ${content.amount} is not a valid number`);
         return false;
     }
 
     content.amount = Number(content.amount.toString().trim());
-    content.assetId = content.assetId === null ? null : Number(content.assetId.toString().trim());
+    content.assetId = content.assetId === null || content.assetId === undefined? null : Number(content.assetId.toString().trim());
 
     return true;
 }
